6ca389ac1f7c678052fd32926c4bcbb10cd7ddfc
ZinedineMess <zinedinemessahel@hotmail.com>
2021-04-29 17:12:36 +0200

    feat: ajout de la classe Appliance

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ import {
 import Recipes from './Recipes.js';
 import SearchBox from './SearchBox.js';
 import Ingredients from './filters/Ingredients.js';
+import Appliance from './filters/Appliance.js';
 
 (function builder() {
     let data = recipes;
     new Recipes().displayRecipes(data);
     new SearchBox().search(data);
     new Ingredients().init(data);
+    new Appliance().init(data);
 }) ();
